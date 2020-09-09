36ea9391e03cd2c374e6554830ef96f1e96e794e
yyfc <592394158@qq.com>
2020-09-09 10:09:07 +0800

    v-1.0.3.2

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 // 配置请求的跟路径
-axios.defaults.baseURL = 'http://localhost:8080/'
+axios.defaults.baseURL = process.env.API_ROOT
 // #  配置 超时时间
 axios.defaults.timeout = 2500
 // home栏
